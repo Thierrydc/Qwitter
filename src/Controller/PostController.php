f2074cc3ba58bfcaf54e30fe2fc9eaf7a1a888aa
ThierryDC <dthierry.talis.bs@gmail.com>
2021-06-04 14:08:22 +0200

    add pagination

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -7,6 +7,7 @@ use App\Entity\Post;
 use App\Form\CommentFormType;
 use App\Form\PostType;
 use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,10 +30,12 @@ class PostController extends AbstractController
     /**
      * @Route("/", name="post_index", methods={"GET"})
      */
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $posts = $paginator->paginate($postRepository->findByAll(), $request->query->getInt('page', 1), 10);
+
         return $this->render('post/index.html.twig', [
-            'posts' => $postRepository->findAll(),
+            'posts' => $posts,
         ]);
     }
 
